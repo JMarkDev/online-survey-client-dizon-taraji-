b478efaa55d1912197ab15376da1ae93f81d8a11
Josiel Mark M. Seroy <110972366+JMarkDev@users.noreply.github.com>
2024-04-30 22:19:35 +0800

    Update FilterByCourseChart.jsx

--- src/components/FilterByCourseChart.jsx
+++ src/components/FilterByCourseChart.jsx
@@ -154,7 +154,7 @@ const FilterByCourseChart = ({ surveyData }) => {
           <option value="">Select Gender</option>
           <option value="Male">Male</option>
           <option value="Female">Female</option>
-          <option value="Non-binary">Non-binary</option>
+          <option value="Non-Binary">Non-binary</option>
         </select>
       </div>
       <div id="chart">
